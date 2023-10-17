dc990e37acc1f888fafa3c6534199300bbc3b459
Divya124-di <121281987+Divya124-di@users.noreply.github.com>
2023-10-17 12:12:58 +0530

    add vdos, images to website

---
+++ js/app.js
@@ -0,0 +1,65 @@
+const targetDiv1 = document.getElementById('answer1');
+const btn1 = document.getElementById('ques-1');
+btn1.onclick = function(){
+    if(targetDiv1.style.display !== "none"){
+        targetDiv1.style.display = "none";
+    }
+    else{
+        targetDiv1.style.display = "block";
+    }
+}
+
+const targetDiv2 = document.getElementById('answer2');
+const btn2 = document.getElementById('ques-2');
+btn2.onclick = function(){
+    if(targetDiv2.style.display !== "none"){
+        targetDiv2.style.display = "none";
+    }
+    else{
+        targetDiv2.style.display = "block";
+    }
+}
+
+const targetDiv3 = document.getElementById('answer3');
+const btn3 = document.getElementById('ques-3');
+btn3.onclick = function(){
+    if(targetDiv3.style.display !== "none"){
+        targetDiv3.style.display = "none";
+    }
+    else{
+        targetDiv3.style.display = "block";
+    }
+}
+
+const targetDiv4 = document.getElementById('answer4');
+const btn4 = document.getElementById('ques-4');
+btn4.onclick = function(){
+    if(targetDiv4.style.display !== "none"){
+        targetDiv4.style.display = "none";
+    }
+    else{
+        targetDiv4.style.display = "block";
+    }
+}
+
+const targetDiv5 = document.getElementById('answer5');
+const btn5 = document.getElementById('ques-5');
+btn5.onclick = function(){
+    if(targetDiv5.style.display !== "none"){
+        targetDiv5.style.display = "none";
+    }
+    else{
+        targetDiv5.style.display = "block";
+    }
+}
+
+const targetDiv6 = document.getElementById('answer6');
+const btn6 = document.getElementById('ques-6');
+btn6.onclick = function(){
+    if(targetDiv6.style.display !== "none"){
+        targetDiv6.style.display = "none";
+    }
+    else{
+        targetDiv6.style.display = "block";
+    }
+}
